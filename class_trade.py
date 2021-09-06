ad60f837bfea96a7cb3053d378e4353912c24585
Małgorzata Żebrowska <mal.zebro95@gmail.com>
2021-09-06 16:11:53 +0200

    com and visualization

--- class_trade.py
+++ class_trade.py
@@ -3,9 +3,10 @@ Software prepared for initial signal processing and basic statistics generation 
 Module contains main core of initial analysis of trade and functions which uploads needed input data from .csv file.
 Predefined points used in get_com_distance_list can be generated and saved to file -> class PredefinedPoints
 """
+import os
 
 import pandas as pd
-import math
+import matplotlib.pyplot as plt
 import settings
 
 
@@ -59,12 +60,11 @@ class Trade:
         """
         data['x'] = data['x']*factor
         data['y'] = data['y']*factor
-
         return data
 
     def get_mean_velocity(self, data):
         """
-        Function return mean velocity of trace based on x,y coordinates and time vector
+        Function calculate mean velocity of drawn trace based on (x,y) coordinates and time vector
         :param data: DataFrame with coordinates and time
         :return: data
         """
@@ -73,8 +73,50 @@ class Trade:
         velocity_y = diff_data['y'] / diff_data['time']
         mean_velocity_temp = velocity_x.pow(2).add(velocity_y.pow(2))
         mean_velocity = mean_velocity_temp.pow(0.5)
-
         return mean_velocity
+
+    def get_com(self, data):
+        """
+        Fuction calculate center of mass of signal as a list with two elements [x_com, y_com]
+        :param data:
+        :return:
+        """
+        x_com = data['x'].mean()
+        y_com = data['y'].mean()
+        return [x_com, y_com]
+
+    def get_com_distance_list(self, predefined_points, com):
+        """
+        Function generate list of distances between given (x,y) coordinates and center of mass from original .csv data
+        :param predefined_points: DataFrame with two columns ['x' and 'y']
+        :param com: center of mass as a list with x, y coordinates
+        :return: list
+        """
+        distances_x = predefined_points['x'].subtract(com[0]).pow(2)
+        distances_y = predefined_points['y'].subtract(com[1]).pow(2)
+        distances_temp = distances_x.add(distances_y)
+        distances = distances_temp.pow(0.5).tolist()
+        return distances
+
+    def get_plot(self, data, predefined_points, com, filename):
+        """
+        Function to visualize trace, center of mass and predefined points
+        :param data: DataFrame with x,y coordinates
+        :param predefined_points: dataframe with predefined points
+        :param com: center of mass as a list with x,y
+        :param filename: string with extension
+        :return:
+        """
+
+        fig, ax = plt.subplots()
+        ax.plot(data['x'], data['y'])
+        ax.scatter(predefined_points['x'], predefined_points['y'])
+        ax.scatter(com[0], com[1])
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.legend(['data', 'predefined', 'com'])
+        plt.savefig(os.path.join(settings.REPORTS_PATH, filename))
+        plt.show()
 
 
 def main():
@@ -83,8 +125,11 @@ def main():
     data_object = Trade(file_path=file)
     dane = data_object.data
     converted = data_object.get_timestamp_from_string(data=dane, col_str_index=0)
-    test_scale = data_object.scale_coordinates(converted)
+    test_scale = data_object.scale_coordinates(converted, factor=10.0)
     test_velocity = data_object.get_mean_velocity(test_scale)
+    test_com = data_object.get_com(test_scale)
+    test_distance = data_object.get_com_distance_list(test_scale, test_com)
+    data_object.get_plot(converted, test_scale, test_com, filename='interview_task_plot.jpg')
 
     print('Test running... data loaded from csv')
 
