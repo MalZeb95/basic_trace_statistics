9c64652136ddb2e14c5e7ef73786df93378bb3ee
Małgorzata Żebrowska <mal.zebro95@gmail.com>
2021-09-06 14:26:24 +0200

    scale function

--- class_trade.py
+++ class_trade.py
@@ -18,6 +18,8 @@ class Trade:
     """
     def __init__(self, file_path, separator='\t'):
         self.data = pd.read_csv(file_path, sep=separator)
+        self.x_coordinates = self.data['x']
+        self.y_coordinates = self.data['y']
 
     def get_timestamp_from_string(self, data, col_str_index=0):
         """
@@ -33,6 +35,31 @@ class Trade:
         data = data.rename(columns={string_column: "time"})
         return data
 
+    def resample_data(self, data, origin_freq_hz=50, expected_freq_hz=20):
+        """
+        Function convert signal sampled with origin_freq_hz frequency into expected_freq_hz
+        :param data: data as DataFrame
+        :param origin_freq_hz: int value of origin sampling frequency in Hz
+        :param expected_freq_hz: int value of expected sampling frequency in Hz
+        :return: data
+        """
+        # dorobić tą funkcję lepiej Bo nie działa!!!!!!!!!!!!!!!!!!!!!
+        test = data.resample('50ms')
+        print('test resample')
+        return test
+
+    def scale_coordinates(self, data, factor=10.0):
+        """
+        Fuction which scales coordinates by multiplying x,y values with factor
+        :param data: data in DataFrame
+        :param factor: scalar for scaling coordinates
+        :return: data
+        """
+        data['x'] = data['x']*factor
+        data['y'] = data['y']*factor
+
+        return data
+
 
 def main():
 
@@ -40,6 +67,8 @@ def main():
     data_object = Trade(file_path=file)
     dane = data_object.data
     converted = data_object.get_timestamp_from_string(data=dane, col_str_index=0)
+    test = data_object.scale_coordinates(converted)
+
     print('Test running... data loaded from csv')
 
 
