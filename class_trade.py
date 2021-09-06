94031d0a91d599b8a418668d8750df7dd32c185b
Małgorzata Żebrowska <mal.zebro95@gmail.com>
2021-09-06 13:30:39 +0200

    get time function

--- class_trade.py
+++ class_trade.py
@@ -16,8 +16,22 @@ class Trade:
     - calculation of the mean velocity, center of mass(COM) and distance between COM and predefined points,
     - visualisation of the trace, center of mass and predefined points.
     """
-    def __init__(self, file_path, separator='\t', index_column=0):
-        self.data = pd.read_csv(file_path, sep=separator, index_col=index_column)
+    def __init__(self, file_path, separator='\t'):
+        self.data = pd.read_csv(file_path, sep=separator)
+
+    def get_timestamp_from_string(self, data, col_str_index=0):
+        """
+        Function convert first column with string (info about date and time sample) into time vector
+        :param data: DataFrame with signals and string column(data and time)
+        :param col_str_index: index of string column
+        :return: data
+        """
+        column_names = data.columns
+        string_column = column_names[col_str_index]
+        data[string_column] = pd.to_datetime(data[string_column], format='%Y-%m-%d %H:%M:%S.%f')
+        data[string_column] = data[string_column].dt.strftime('%S.%f')
+        data = data.rename(columns={string_column: "time"})
+        return data
 
 
 def main():
@@ -25,6 +39,7 @@ def main():
     file = settings.DATA_PATH
     data_object = Trade(file_path=file)
     dane = data_object.data
+    converted = data_object.get_timestamp_from_string(data=dane, col_str_index=0)
     print('Test running... data loaded from csv')
 
 
